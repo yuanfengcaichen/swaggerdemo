97cacee7ee25458748b874d08ae972e1f91fc71f
yuanfengcaichen <1634472659@qq.com>
2021-01-19 16:17:14 +0800

    m

--- src/main/java/club/codeqi/Controller/test.java
+++ src/main/java/club/codeqi/Controller/test.java
@@ -1,8 +1,10 @@
 package club.codeqi.Controller;
+import net.sf.sevenzipjbinding.*;
+import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
+import net.sf.sevenzipjbinding.simple.ISimpleInArchive;
+import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class test {
     public static void main(String[] args) {
@@ -82,5 +84,42 @@ public class test {
         } catch (FileNotFoundException e) {
             System.out.println("readfile()   Exception:" + e.getMessage());
         }
+    }
+
+    public static boolean un7z(String file7zPath, final String outPutPath, String passWord) throws Exception {
+        boolean flag = false;
+        IInArchive archive;
+        RandomAccessFile randomAccessFile;
+        randomAccessFile = new RandomAccessFile(file7zPath, "r");
+        archive = SevenZip.openInArchive(null, new RandomAccessFileInStream(randomAccessFile), passWord);
+        int numberOfItems = archive.getNumberOfItems();
+        ISimpleInArchive simpleInArchive = archive.getSimpleInterface();
+        //for (final ISimpleInArchiveItem it\em : simpleInArchive.getArchiveItems()) {
+        ISimpleInArchiveItem item=simpleInArchive.getArchiveItems()[1];
+        if (!item.isFolder()) {
+            ExtractOperationResult result;
+            result = item.extractSlow(new ISequentialOutStream() {
+                public int write(byte[] data) throws SevenZipException {
+                    try {
+                        String parentFilePath = outPutPath;
+                        if (!new File(parentFilePath).exists()) {
+                            new File(parentFilePath).mkdirs();
+                        }
+                        IOUtils.write(data, new FileOutputStream(new File(outPutPath + File.separator + item.getPath()), true));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return data.length; // Return amount of consumed
+                }
+            }, passWord);
+            if (result == ExtractOperationResult.OK) {
+                flag=true;
+            } else {
+                flag=false;
+            }
+        }
+        archive.close();
+        randomAccessFile.close();
+        return flag;
     }
 }
